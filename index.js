bba27d855212caf8f71d56cfa0db8b58adddfdb9
Aayush501 <shrivastavashu501@gmail.com>
2025-03-02 03:49:52 +0530

    Added

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const app = express();
 
 
 const allowedOrigins = [
-  "https://shopwan.vercel.app/", // ✅ Replace with your actual frontend domain
-  "https://shopwanadmin.vercel.app/"
+  "https://shopwan.vercel.app", // ✅ Replace with your actual frontend domain
+  "https://shopwanadmin.vercel.app"
 ];
 
 // Middleware setup
