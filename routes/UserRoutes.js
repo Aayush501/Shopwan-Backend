3d543c1ffeeb1e987895af01c27e4b472a8fad0e
Aayush501 <shrivastavashu501@gmail.com>
2025-03-18 00:28:25 +0530

    18-03-25 -> 1 -> 12:28

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -138,6 +138,9 @@ router.get("/cart", async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
+    console.log(user);
+    console.log(user.cart);
+
     res.json({ cart: user.cart });
   } catch (error) {
     res.status(500).json({ message: "Error fetching cart", error });
