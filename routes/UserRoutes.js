6fd70b802efe65ce9a8192f7f6b784ceba843991
Aayush501 <shrivastavashu501@gmail.com>
2025-03-18 20:31:57 +0530

    18-03-25 -> 17 -> 8:31 pm

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -65,10 +65,13 @@ router.post(
       if (!user) return res.status(404).json({ message: "User not found" });
       console.log(user);
 
-      if (user.cart.includes(product)) {
-        alert("Product Already In Your Cart");
-        return res.status(400).json({ message: "Product already in cart" });
-      }
+      user.cart.forEach((p) => {
+        if (p.uid === productId) {
+          alert("Product Already In Your Cart");
+          return res.status(400).json({ message: "Product already in cart" });
+        }
+      });
+      
       console.log("product not in cart.");
 
       user.cart.push(product);
