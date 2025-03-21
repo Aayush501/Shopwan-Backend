0eabdf32263288b4020f2aab6367b672297d09da
Aayush501 <shrivastavashu501@gmail.com>
2025-03-22 02:45:32 +0530

    22-03-25 -> 3 ->  2:45 am

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -78,13 +78,13 @@ router.get("/getAddresses/:email", async (req, res) => {
       return res.status(400).json({ message: "Email is required" });
     }
 
-    const user = await User.findOne({ email }, "address");
+    const user = await User.findOne({ email: email }, "addresses");
 
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
 
-    res.status(200).json({ message: "Addresses fetched successfully", addresses: user.address });
+    res.status(200).json({ message: "Addresses fetched successfully", addresses: user.addresses });
   } catch (error) {
     console.error("Error fetching addresses:", error);
     res.status(500).json({ message: "Internal server error", error: error.message });
